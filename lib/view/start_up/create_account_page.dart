d84968b9f34071ecc3964b2c86bb6d1d9d518da4
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2022-10-17 11:49:28 +0900

    アカウント作成　UI

--- lib/view/start_up/create_account_page.dart
+++ lib/view/start_up/create_account_page.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CreateAccountPage extends StatefulWidget {
   const CreateAccountPage({super.key});
@@ -13,6 +15,15 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
   TextEditingController selfIntroductionController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController passController = TextEditingController();
+  File? image;
+  ImagePicker picker = ImagePicker();
+
+  Future<void> GetImageFromGallery() async {
+    final pickedfile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedfile != null) {
+      image = File(pickedfile.path);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,59 +32,87 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
         backgroundColor: Colors.transparent,
         elevation: 0,
         iconTheme: IconThemeData(color: Colors.black),
-        title: Text('新規登録', 
-        style: TextStyle(color: Colors.black),),
+        title: Text(
+          '新規登録',
+          style: TextStyle(color: Colors.black),
+        ),
       ),
-      body: Container(
-        width: double.infinity,
-        child: Column(
-          children: [
-            SizedBox(height: 30,),
-            CircleAvatar(
-              radius: 40,
-              child: Icon(Icons.add),
-            ),
-            Container(
-              width: 300,
-              child: TextField(
-                controller: nameController,
-                decoration: InputDecoration(
-                  hintText: '名前'),
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 30,
               ),
-            ),
-            Container(
-              width: 300,
-              child: TextField(
-                controller: userIdController,
-                decoration: InputDecoration(
-                  hintText: 'ユーザーID'),
+              GestureDetector(
+                onTap: () {
+                  GetImageFromGallery();
+                },
+                child: CircleAvatar(
+                  foregroundImage: image == null ? null: FileImage(image!),
+                  radius: 40,
+                  child: Icon(Icons.add),
+                ),
               ),
-            ),
-            Container(
-              width: 300,
-              child: TextField(
-                controller: selfIntroductionController,
-                decoration: InputDecoration(
-                  hintText: '自己紹介'),
+              Container(
+                width: 300,
+                child: TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(hintText: '名前'),
+                ),
               ),
-            ),
-            Container(
-              width: 300,
-              child: TextField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  hintText: 'メールアドレス'),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Container(
+                  width: 300,
+                  child: TextField(
+                    controller: userIdController,
+                    decoration: InputDecoration(hintText: 'ユーザーID'),
+                  ),
+                ),
               ),
-            ),
-            Container(
-              width: 300,
-              child: TextField(
-                controller: passController,
-                decoration: InputDecoration(
-                  hintText: 'パスワード'),
+              Container(
+                width: 300,
+                child: TextField(
+                  controller: selfIntroductionController,
+                  decoration: InputDecoration(hintText: '自己紹介'),
+                ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Container(
+                  width: 300,
+                  child: TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(hintText: 'メールアドレス'),
+                  ),
+                ),
+              ),
+              Container(
+                width: 300,
+                child: TextField(
+                  controller: passController,
+                  decoration: InputDecoration(hintText: 'パスワード'),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if (nameController.text.isNotEmpty &&
+                        userIdController.text.isNotEmpty &&
+                        selfIntroductionController.text.isNotEmpty &&
+                        emailController.text.isNotEmpty &&
+                        passController.text.isNotEmpty &&
+                        image != null) {
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: Text('アカウントを作成')),
+            ],
+          ),
         ),
       ),
     );
